0cdc042404d0c4fef606f95e21c5d22f239b02f5
Nimos7 <jakub.jachimekk@gmail.com>
2022-09-05 22:13:22 +0200

    feat: property test

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -13,5 +13,6 @@ import { EmployeeModel } from '../../model/employee.model';
 export class EmployeeListComponent {
   constructor(private _httpClient: HttpClient) {}
   data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json') ;
+  title: string = 'Pracownicy ' ;
 
   }
