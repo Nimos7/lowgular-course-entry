28c5ed060356769e71f99e282ff52bf8271ba203
Nimos7 <jakub.jachimekk@gmail.com>
2022-10-13 23:44:36 +0200

    feat: part 4 exercise 1 - add homepage+ form and list routes from homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './ui/employee-form/employee-form.component';
+import { HomePageComponent } from './ui/home-page/home-page.component';
+import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
+import { EmployeeFormComponentModule } from './ui/employee-form/employee-form.component-module';
+import { HomePageComponentModule } from './ui/home-page/home-page.component-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'employee-list', component: EmployeeListComponent }, { path: 'employee-create', component: EmployeeFormComponent }, { path: '', component: HomePageComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, EmployeeFormComponentModule, HomePageComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
